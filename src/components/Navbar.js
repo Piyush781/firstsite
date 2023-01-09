a2da4558fcd3bbe0f877c1118593c5464d05dfbe
Piyush781 <95937381+Piyush781@users.noreply.github.com>
2023-01-09 16:39:00 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,10 +12,10 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auhref mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-a active" aria-current="page" href="#about">Home</a>
+          <a className="nav-a active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-a" href="#home">About</a>
+          <a className="nav-a" href="#">About</a>
         </li>
          
           </ul>
