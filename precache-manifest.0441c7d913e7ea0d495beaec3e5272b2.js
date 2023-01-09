374e9c70412bf6744220dd8220d5f152c68ef49c
Piyush781 <95937381+Piyush781@users.noreply.github.com>
2023-01-09 16:12:41 +0530

    Updates

---
+++ precache-manifest.0441c7d913e7ea0d495beaec3e5272b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f754d9651aae1b60aea",
+    "url": "/my-app/static/css/main.d4328b07.chunk.css"
+  },
+  {
+    "revision": "5f754d9651aae1b60aea",
+    "url": "/my-app/static/js/main.5f754d96.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/my-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/my-app/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8b16e4071cc3418912ee",
+    "url": "/my-app/static/js/runtime~main.8b16e407.js"
+  },
+  {
+    "revision": "06737bfa77bffc721ef53a177c529965",
+    "url": "/my-app/index.html"
+  }
+];
